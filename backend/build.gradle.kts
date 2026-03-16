eb53cbf8b993adb02924e35bf4bad653a4d7ee1f
antholim <139510082+antholim@users.noreply.github.com>
2026-03-16 15:12:35 -0400

    Added

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "com.example"
@@ -36,4 +38,20 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "SOEN345-PROJECT-backend")
+		property("sonar.projectName", "SOEN345 Backend")
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+	}
 }
